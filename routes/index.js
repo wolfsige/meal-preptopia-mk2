a9125871cc9b1de43946b7da6fa76eef5c148f2a
Wolf <d.wolftripp@gmail.com>
2023-03-16 10:53:18 -0700

    change title, add partials, rework meals/index view

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = Router()
 
 // GET localhost:3000/
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Meal Preptopia' })
 })
 
 export { router }
