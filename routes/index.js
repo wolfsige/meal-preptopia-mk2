d840ddc7468e8da8b83c7149e967428ba3ce24f4
Wolf <d.wolftripp@gmail.com>
2023-03-30 13:39:54 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ const router = Router()
 
 // GET localhost:3000/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Meal Preptopia' })
+  res.render('index', { 
+    title: 'Meal Preptopia',
+    user: req.user
+  })
 })
 
 export { router }
